b086c0f379671c06c05089740dba9e649d75e939
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2019-06-03 16:35:39 +0900

    Update Defines.h

--- Defines.h
+++ Defines.h
@@ -5,7 +5,7 @@
 #define STAGE_WIDTH	20
 #define STAGE_HEIGHT	18
 #define FIELD_WIDTH	10
-#define FIELD_HEIGHT 18
+#define FIELD_HEIGHT  18
 #define PART_SIZE	8
 #define ZOOM_RATE	3
 #define WORD_SIZE	11
